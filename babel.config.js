f6494f85e1ceceefc7f1524a2087068e71a85882
dsfsk <120457328+dsfsk@users.noreply.github.com>
2022-12-20 18:31:37 +0800

    身份认证

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,19 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"]
-  };
-};
+    plugins: [
+      'nativewind/babel',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@components': './src/components',
+            '@services': './src/services',
+          },
+        },
+      ],
+    ],
+  }
+}
--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,21 +1,32 @@
-import {Text, View} from 'react-native'
-import React, {useEffect} from 'react'
-import {useAppDispatch, useAppSelector} from "../../store";
-import {updateName} from "../../appSlice";
+import { Alert, Text, View } from 'react-native'
+import React from 'react'
+import { MyAuthentication } from '@components/MyAuthentication'
 
 const Login = () => {
-    const name = useAppSelector((state) => state.app.name)
-    const dispatch = useAppDispatch()
-
-    console.log(`app name ${name}`)
-    useEffect(() => {
-        dispatch(updateName('newApp'))
-    }, [])
-    return (
-        <View>
-            <Text>Login</Text>
-        </View>
-    )
+  return (
+    <View>
+      <Text>Login in</Text>
+      <MyAuthentication
+        title="验证"
+        showAuthenticationType={true}
+        authenticateOption={{
+          promptMessage: '进行验证',
+          // 禁止人脸不可用的情况下使用密码
+          disableDeviceFallback: false,
+        }}
+        onAuthenticate={(result) => {
+          if (result.success) {
+            Alert.alert('验证成功')
+          } else {
+            Alert.alert('验证失败')
+          }
+        }}
+        onAuthenticateError={(err) => {
+          Alert.alert('验证错误', err.message)
+        }}
+      />
+    </View>
+  )
 }
 
 export default Login
