195655a8319f55048ed2862089afa4232b4a2345
0xkeykey <keysafewallet@hotmail.com>
2023-01-10 23:03:34 +0800

    feat: add confirm code filed
    s

--- src/components/EmailVerify.tsx
+++ src/components/EmailVerify.tsx
@@ -1,16 +1,21 @@
 import { View, Text, StyleSheet, SafeAreaView, TextInput } from 'react-native'
-import React, { useState } from 'react'
+import React, { FC, useState } from 'react'
 import FullScreenContainer from './Container'
 import CButton from './basics/Button'
 import { createGenericAccount } from '../services/generic'
 import { useNavigation } from '@react-navigation/native'
+import { TEmailVerifyType } from '../types'
+import InterText from './basics/Button/InterText'
+import MCIcons from 'react-native-vector-icons/Fontisto';
+import { CodeField, useBlurOnFulfill, useClearByFocusCell } from 'react-native-confirmation-code-field'
+import VerifyCodeInput from './VerifyCodeInput'
 
 enum EStep {
   FILL_EMAIL,
   FILL_CODE,
 }
-const text = [
-  {
+const text = {
+  "REGISTER": [{
     title: "Welcome to Keysafe",
     subTitle: "This email address is required to access your crypto accounts",
     desc: "By signing up, you are agreeing to our Terms and Privacy policy",
@@ -19,55 +24,76 @@ const text = [
     title: "Verify your email",
     subTitle: "Enter the confirmation code we just emailed to you",
     desc: "",
-  }
-]
-const EmailVerify = () => {
+  }],
+  "LOGIN": [
+    {
+      title: "Welcome to Keysafe",
+      subTitle: "This email address is required to access your crypto accounts",
+      desc: "By signing up, you are agreeing to our Terms and Privacy policy",
+    },
+    {
+      title: "Verify your email",
+      subTitle: "Enter the confirmation code we just emailed to you",
+      desc: "",
+    }
+  ]
+}
+interface IEmailVerify {
+  type: TEmailVerifyType
+}
+const EmailVerify: FC<IEmailVerify> = ({ type = 'REGISTER' }) => {
   const [email, setEmail] = useState('keysafe@gmail.com')
   const [code, setCode] = useState('')
   const [step, setStep] = useState(EStep.FILL_EMAIL)
   const navigation = useNavigation()
+
   const onNextPress = () => {
     if (step === EStep.FILL_EMAIL) {
       // request email verify code
       setStep(EStep.FILL_CODE)
-    } else {
-      // createAccount
-      const seed = createGenericAccount()
-      navigation.navigate('FaceId')
     }
-
+  }
+  const onVerify = () => {
+    const seed = createGenericAccount()
+    navigation.navigate('FaceId')
   }
   return (
     <FullScreenContainer>
       <View className='py-8'>
-        <Text className='text-2xl py-4  font-bold'>
+        <InterText passedClassName='text-2xl py-4'>
           {
-            text[step].title
+            text[type][step].title
           }
-        </Text>
-        <Text className=' text-base'>
-          {text[step].subTitle}
-        </Text>
+        </InterText>
+        <InterText passedClassName='text-base text-[#A2A0A8]' weight='300'>
+          {text[type][step].subTitle}
+        </InterText>
       </View>
       <SafeAreaView>
         {
-          step === EStep.FILL_EMAIL ? <TextInput
-            className='h-12 border text-base rounded-sm px-4 '
-            onChangeText={setEmail}
-            value={email}
-          /> : <TextInput
-            className='h-12 border text-base  rounded-sm px-4 '
-            onChangeText={setCode}
-            value={code}
-          />
+          step === EStep.FILL_EMAIL ? <View className='bg-[#F9F9FA] flex-row items-center pl-4 rounded-2xl'>
+            <MCIcons name={'male'} size={24}></MCIcons>
+            <TextInput
+              className='h-14	text-base px-4 ' style={{ lineHeight: 0, fontFamily: "Inter_400" }}
+              onChangeText={setEmail}
+              value={email}
+            />
+          </View> : <VerifyCodeInput onComplete={setCode} />
         }
       </SafeAreaView>
       <View className='mt-6 pb-24'>
-        <Text className='text-sm '>
-          {text[step].desc}
-        </Text>
+        <InterText passedClassName='text-sm text-gray-100' weight='300'>
+          {text[type][step].desc}
+        </InterText>
       </View>
-      <CButton passedClassName='w-full' onPress={onNextPress} title={"Next"} />
+      <View>
+        {
+          step === EStep.FILL_EMAIL && <CButton theme='dark' passedClassName='w-full' onPress={onNextPress} >Let’s go!</CButton>
+        }
+        {
+          step === EStep.FILL_CODE && <CButton theme='dark' disabled={code.length !== 6} passedClassName='w-full' onPress={onVerify} >Verify</CButton>
+        }
+      </View>
     </FullScreenContainer >
 
   )
