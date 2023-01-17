2b136a05cf1e94514e216eabddb026db64cf02af
dsfsk <120457328+dsfsk@users.noreply.github.com>
2023-01-18 02:58:50 +0800

    register

--- src/services/generic.ts
+++ src/services/generic.ts
@@ -1,17 +1,35 @@
-import "react-native-get-random-values"
+import 'react-native-get-random-values'
 
 // Import the the ethers shims (**BEFORE** ethers)
-import "@ethersproject/shims"
-
+import '@ethersproject/shims'
 
 // Import the ethers library
-import { Wallet } from "ethers";
-import { getMd5 } from "./crypto";
+import { Wallet } from 'ethers'
+import { getMd5, myCrypto } from './crypto'
+
 export const createGenericAccount = async () => {
     const walletInfo = Wallet.createRandom().mnemonic
-    const digest = await getMd5(walletInfo.phrase);
+    const digest = await getMd5(walletInfo.phrase)
     return {
         user_id: digest,
-        mnemonic: walletInfo.phrase
+        mnemonic: walletInfo.phrase,
     }
 }
+
+export class AccountFactory {
+    async createRandom() {
+        const wallet = Wallet.createRandom()
+        return this.wallet2account(wallet)
+    }
+
+    private async wallet2account(wallet: Wallet) {
+        const { publicKey } = wallet
+        return {
+            mnemonic: wallet.mnemonic.phrase,
+            privateKey: wallet.privateKey,
+            publicKey,
+        }
+    }
+}
+
+export const accountFactory = new AccountFactory()
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,23 @@
-import {configureStore} from "@reduxjs/toolkit";
-import {appReducer} from "./appSlice";
-import {addressReducer} from "./addressSlice";
-import {tokenReducer} from "./tokenSlice";
-import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { appReducer } from "./appSlice";
+import { addressReducer } from "./addressSlice";
+import { tokenReducer } from "./tokenSlice";
+import { accountReducer } from "./accountSlice";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
-    reducer: {
-        app: appReducer,
-        address: addressReducer,
-        token: tokenReducer
-    }
-})
+  reducer: {
+    app: appReducer,
+    account: accountReducer,
+    address: addressReducer,
+    token: tokenReducer
+  }
+});
 
 type AppDispatch = typeof store.dispatch
 
-export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppDispatch: () => AppDispatch = useDispatch;
 
 export type RootState = ReturnType<typeof store.getState>
 
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
